d51eb4fee9771adda8b9cdc13c31fca0ee10f193
shivamparihar12 <parihar.shivam0789@gmail.com>
2024-01-02 16:43:31 +0530

    headlines-fragment setuped

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val API_KEY = "ae2770c6301d40c2a74646daf42f2dba"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_ITEM_DELAY = "500L"
+        const val SEARCH_NEWS_ITEM_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
     }
 }
